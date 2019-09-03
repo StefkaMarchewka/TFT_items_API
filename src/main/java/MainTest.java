d568719a017634ac94b74d361d75b5bf17e34e60
PatrykTopolski <venirro@gmail.com>
2019-09-03 14:11:49 +0200

    ordinary item

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,8 +1,38 @@
+import model.OrdinaryItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainTest {
 
     public static void main(String[] args) {
 
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("tft_items");
+        EntityManager em = emf.createEntityManager();
+
+        fillDB(em);
+        em.clear();
+
+        em.close();
+        emf.close();
+
     }
 
 
+    public static void fillDB(EntityManager em){
+
+        Map<String, String> bFAndBF = new HashMap<String, String>();
+        bFAndBF.put("bf Sword", "Infinity edge");
+        OrdinaryItem BFSword = new OrdinaryItem("BF Sword", "Attack damage", "+20", bFAndBF);
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(BFSword);
+        transaction.commit();
+        System.out.printf("added new item");
+    };
 }
