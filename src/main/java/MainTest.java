2eccadf7949ccfa0571fcd2d24a6d5bfd01c36cc
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-09-03 12:40:40 +0200

    git ignore

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,13 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainTest {
+
+
+}
