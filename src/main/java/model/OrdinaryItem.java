069c98fd4eb6fe412a6d795a3e7cfdf932f28486
PatrykTopolski <venirro@gmail.com>
2019-09-03 13:13:50 +0200

    ordinary item

--- src/main/java/model/OrdinaryItem.java
+++ src/main/java/model/OrdinaryItem.java
@@ -1,20 +1,37 @@
 package model;
 
-import java.util.HashMap;
+import org.eclipse.persistence.annotations.PrimaryKey;
 
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
 public class OrdinaryItem {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    int id;
     private String name;
     private String statistic_name;
     private String ammount;
-    private HashMap matches;
 
-    public OrdinaryItem(String name, String statistic_name, String ammount){
+
+    @ElementCollection
+    private Map<String, String> matches;
+
+    public OrdinaryItem() {
+    }
+
+    public OrdinaryItem(String name, String statistic_name, String ammount, Map matches){
         this.ammount = ammount;
         this.name = name;
         this.statistic_name = statistic_name;
+        this.matches = matches;
     }
 
     public void setMatches(HashMap matches) {
         this.matches = matches;
+
     }
 }
