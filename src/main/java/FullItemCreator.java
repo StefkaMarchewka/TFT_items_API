4e0a2c1afc3f43591a7a0f6751b829d0700a8169
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-09-04 10:00:27 +0200

    more items created

--- src/main/java/FullItemCreator.java
+++ src/main/java/FullItemCreator.java
@@ -21,7 +21,19 @@ public class FullItemCreator {
         createFullItem("Youmuu’s Ghostblade", "Wearer is also an Assassin", "B.F. Sword", "Spatula");
         createFullItem("The Bloodthirster", "Attacks heal for 40% of damage", "B.F. Sword", "Negatron Cloak");
         createFullItem("Zeke's Herald", "On start of combat, allies 2 spaces to the left and right gain +15% Attack Speed.", "B.F. Sword", "Giant’s Belt");
-
+        createFullItem("Locket of the Iron Solari", "On start of combat, allies two spaces to the left and right gain a shield of 250 for 6 seconds.", "Chain Vest", "Needlessly Large Rod ");
+        createFullItem("Thornmail", "Reflect 100% of mitigated damage taken from attacks as magic damage.", "Chain Vest", "Chain Vest");
+        createFullItem("Phantom Dancer:", "Wearer dodges all Critical Strikes.","Chain Vest",  "Recurve Bow");
+        createFullItem("Frozen Heart", "Adjacent enemies lose 25% Attack Speed.", "Chain Vest", "Tear of the Goddess");
+        createFullItem("Sword Breaker", "Attacks have a 25% chance to disarm", "Chain Vest", "Negatron Cloak");
+        createFullItem("Red Buff", "Attacks burn for 20% of max HP over 10s and disable healing", "Chain Vest", "Giant’s Belt");
+        createFullItem("Knight’s Vow", "Wearer is also a Knight", "Chain Vest", "Spatula" );
+        createFullItem("Titanic Hydra", "Attacks deal 10% of the wearer's max HP as splash.", "Giant's Belt", "Recurve Bow");
+        createFullItem("Morellonomicon","Spells deal burn damage equal to 20% of enemy’s max hp over 10s & prevent healing.", "Giant's Belt", "Needlessly Large Rod");
+        createFullItem("Redemption", "At 25% health heal all nearby allies for 1200 HP.", "Giant's Belt", "Tear of the Goddess ");
+        createFullItem("Zephyr", "On start of combat, banish an enemy for 5 seconds", "Giant's Belt", "Negatron Cloak");
+        createFullItem("Warmog's Armor","Wearer regenerates 6% of missing health per second.",  "Giant's Belt", "Giant's Belt");
+        createFullItem("Frozen Mallet", "Wearer is also a Glacial.", "Giant's Belt", "Spatula");
     }
 
     private void createFullItem(String name, String specialEffect, String firstParent, String secondParent){
