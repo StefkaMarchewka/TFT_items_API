6202e88994b168ad2461ca58052a280dcc33d44c
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-09-04 14:18:37 +0200

    proper uri context

--- src/main/java/servlets/CollectionServlet.java
+++ src/main/java/servlets/CollectionServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "collectionservlet", urlPatterns = {"/collections"}, loadOnStartup = 1)
+@WebServlet(name = "collectionservlet", urlPatterns = {"/collections", "collections/item/*"}, loadOnStartup = 1)
 public class CollectionServlet extends HttpServlet {
 
     OrdinaryITemDao oID = new OrdinaryITemDao();
@@ -19,17 +19,22 @@ public class CollectionServlet extends HttpServlet {
         String json = "json";
         String uri = req.getRequestURI();
         System.out.println(uri);
-        if (req.getRequestURI().length() > "/collection".length()){
-            System.out.println("sending item");
-            resp.getWriter().write(oID.getItemByNameToJSON(req.getRequestURI().substring(11)));
-
-
-        }
-        else if (req.getRequestURI().equals("/collection")){
+//        if (req.getRequestURI().length() > "/collection".length()){
+//            System.out.println("sending item");
+//            resp.getWriter().write(oID.getItemByNameToJSON(req.getRequestURI().substring(11)));
+//
+//
+//        }
+       if (req.getRequestURI().equals("/collections")){
             resp.getWriter().write(json);
         }
+       else {
+           resp.getWriter().write("everything else than collection");
+       }
 
         resp.getWriter().write("collection spy");
+
+
 
 
     }
