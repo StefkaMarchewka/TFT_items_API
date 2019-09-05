eb51ecd90f443de155ab2b3b016d6b91203a8884
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-09-05 13:11:28 +0200

    small refactor

--- src/main/java/servlets/FullItemServlet.java
+++ src/main/java/servlets/FullItemServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "collectionservlet", urlPatterns = {"/collections", "/collections/fullitems/*"}, loadOnStartup = 1)
+@WebServlet(name = "collectionservlet", urlPatterns = {"/collections/fullitems/*"}, loadOnStartup = 1)
 public class FullItemServlet extends HttpServlet {
 
     OrdinaryITemDao oID = new OrdinaryITemDao();
@@ -20,32 +20,25 @@ public class FullItemServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String json = "json";
         String uri = req.getRequestURI();
-        System.out.println(uri);
+
+        String[] uriParts = req.getRequestURI().split("/");
+        //System.out.println(uriParts[3] + " dupa");
+        String name = "";
+        String nameToParse = uriParts[3];
+        if(nameToParse.contains("_")){
+            String[] splited = nameToParse.split("_");
+            StringBuilder sb = new StringBuilder();
+            sb.append(splited[0]);
+            sb.append(" ");
+            sb.append(splited[1]);
+
+            name = sb.toString();
+        }else name = uriParts[3];
 
 
-        if (req.getRequestURI().contains("collections/fullitems/")){
-            String[] uriParts = req.getRequestURI().split("/");
-            System.out.println(uriParts[3] + " dupa");
-            String name = "";
-            String nameToParse = uriParts[3];
-            if(nameToParse.contains("_")){
-                String[] splited = nameToParse.split("_");
-                StringBuilder sb = new StringBuilder();
-                sb.append(splited[0]);
-                sb.append(" ");
-                sb.append(splited[1]);
-
-                name = sb.toString();
-            }else name = uriParts[3];
-
-
-            FullItem itemToShow = fullItemDAO.getByName(name);
-            resp.getWriter().write(fullItemDAO.writeJSONFullItem(itemToShow));
-        }
-
-
+        FullItem itemToShow = fullItemDAO.getByName(name);
+        resp.getWriter().write(fullItemDAO.writeJSONFullItem(itemToShow));
 
 
 
