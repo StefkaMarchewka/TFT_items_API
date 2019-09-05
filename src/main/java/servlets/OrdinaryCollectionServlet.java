7feae922a8a6f9528501c5d628dd02574a5b8e3d
PatrykTopolski <venirro@gmail.com>
2019-09-05 13:10:56 +0200

    refactored servlets, now each uri has its own servlet

---
+++ src/main/java/servlets/OrdinaryCollectionServlet.java
@@ -0,0 +1,51 @@
+package servlets;
+
+import dao.OrdinaryITemDao;
+import model.OrdinaryItem;
+import netscape.javascript.JSObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "OrdinaryCollectionservlet", urlPatterns = {"/collections/ordinary"}, loadOnStartup = 1)
+public class OrdinaryCollectionServlet extends HttpServlet {
+
+    OrdinaryITemDao oID = new OrdinaryITemDao();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write(oID.getAllItems());
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List jsonList = (List)  req.getAttribute("collection");
+        List<OrdinaryItem> ordinaryItemList = new ArrayList<>();
+        for (Object object:  jsonList
+        ) {
+            JSObject json = (JSObject) object;
+            OrdinaryItem ordinaryItem = new OrdinaryItem((String) json.getMember("name"),
+                    (String) json.getMember("statistic name"), (String) json.getMember("amount"));
+            ordinaryItemList.add(ordinaryItem);
+        }
+        oID.addItems(ordinaryItemList);
+
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        oID.delateItems();
+
+    }
+
+
+}
---
+++ src/main/java/servlets/OrdinaryItemServlet.java
@@ -0,0 +1,37 @@
+package servlets;
+
+import dao.OrdinaryITemDao;
+import model.OrdinaryItem;
+import netscape.javascript.JSObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+    @WebServlet(name = "OrdinaryItemServlet", urlPatterns = { "collections/ordinary/item/*"}, loadOnStartup = 1)
+    public class OrdinaryItemServlet extends HttpServlet {
+
+        OrdinaryITemDao oID = new OrdinaryITemDao();
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String[] uriParts = req.getRequestURI().split("/");
+            System.out.println(uriParts[3] + "dupa");
+            resp.getWriter().write(oID.getItemByNameToJSON(3));
+        }
+
+        @Override
+        protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        }
+
+        @Override
+        protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        }
+
+
+}
